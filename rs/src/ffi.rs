0f0362b11f03e2b542da06d7a9a67fd9f4ba2e7e
Jonathan Protzenko <protz@microsoft.com>
2025-04-11 16:36:43 -0700

    Proper FFI exports

--- rs/src/ffi.rs
+++ rs/src/ffi.rs
@@ -1,18 +1,57 @@
+use crate::symcrypt_internal::*;
+
+const SYMCRYPT_SHA3_256_RESULT_SIZE: usize = 32;
+
+type PSYMCRYPT_SHA3_256_STATE = *mut SYMCRYPT_SHA3_256_STATE;
+type PCSYMCRYPT_SHA3_256_STATE = *const SYMCRYPT_SHA3_256_STATE;
+
 #[no_mangle]
 pub extern "C" fn SymCryptSha3_256(
-    pb_data: *const u8,
+    pbData: *const u8,
     cbData: usize,
-    pb_result: &mut [u8; SHA3_256_RESULT_SIZE],
-);
+    pbResult: &mut [u8; SYMCRYPT_SHA3_256_RESULT_SIZE],
+) {
+    let data = unsafe { std::slice::from_raw_parts(pbData, cbData) };
+    crate::sha3_256::SymCryptSha3_256(data, cbData, pbResult)
+}
 
 #[no_mangle]
-pub extern "C" fn SymCryptSha3_256Init(p_state: &mut HashState);
+pub extern "C" fn
+SymCryptSha3_256Init(pState: PSYMCRYPT_SHA3_256_STATE) {
+    let state = unsafe { std::slice::from_raw_parts_mut(pState, 1) };
+    crate::sha3_256::SymCryptSha3_256Init(state)
+}
 
 #[no_mangle]
-pub extern "C" fn SymCryptSha3_256Append(p_state: &mut HashState, pb_data: *const u8, cbData: usize);
+pub extern "C" fn
+SymCryptSha3_256Append(
+    pState: PSYMCRYPT_SHA3_256_STATE,
+    pbData: *const u8,
+    cbData: usize)
+{
+    let state = unsafe { std::slice::from_raw_parts_mut(pState, 1) };
+    let data = unsafe { std::slice::from_raw_parts(pbData, cbData) };
+    crate::sha3_256::SymCryptSha3_256Append(state, data, cbData);
+}
 
 #[no_mangle]
-pub extern "C" fn SymCryptSha3_256Result(p_state: &mut HashState, pb_result: &mut [u8; SHA3_256_RESULT_SIZE]);
+pub extern "C" fn
+SymCryptSha3_256Result(
+    pState: PSYMCRYPT_SHA3_256_STATE,
+    pbResult: &mut [u8; SYMCRYPT_SHA3_256_RESULT_SIZE]
+) {
+    let state = unsafe { std::slice::from_raw_parts_mut(pState, 1) };
+    crate::sha3_256::SymCryptSha3_256Result(state, pbResult);
+}
 
 #[no_mangle]
-pub extern "C" fn SymCryptSha3_256StateCopy(p_src: &HashState, p_dst: &mut HashState);
+pub extern "C" fn
+SymCryptSha3_256StateCopy(pSrc: PCSYMCRYPT_SHA3_256_STATE, pDst: PSYMCRYPT_SHA3_256_STATE) {
+    let src = unsafe { std::slice::from_raw_parts(pSrc, 1) };
+    let dst = unsafe { std::slice::from_raw_parts_mut(pDst, 1) };
+    crate::sha3_256::SymCryptSha3_256StateCopy(src, dst);
+}
+
+#[no_mangle]
+pub extern "C" fn
+SymCryptModuleInit() {}
